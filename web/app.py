3150bd5412a8413538fd3e3b932d312d7699ac85
Róbert Fjölnir Birkisson <rfb2@hi.is>
2024-02-07 14:47:46 +0100

    DB now working

--- web/app.py
+++ web/app.py
@@ -1,5 +1,26 @@
+import os
+import psycopg
 from flask import Flask
+import dotenv
+
 app = Flask(__name__)
+
+dotenv.load_dotenv()
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+
+connect_string = "postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}"
+
+with psycopg.connect(connect_string) as conn:
+    with conn.cursor() as cur:
+        cur.execute(
+            '''CREATE TABLE IF NOT EXISTS products (id serial \
+            PRIMARY KEY, name varchar(100), price float);''')
+        cur.execute(
+            '''INSERT INTO products (name, price) VALUES \
+            ('Apple', 1.99), ('Orange', 0.99), ('Banana', 0.59);''')
+        conn.commit()
 
 
 @app.route('/')
@@ -9,7 +30,10 @@ def hello_world():
 
 @app.route('/lol')
 def lolol():
-    return '<h1>Uhhhh, lolol<h1>'
+    with psycopg.connect(connect_string) as conn:
+        data = conn.execute('''SELECT * FROM products''').fetchall()
+
+    return data
 
 
 if __name__ == '__main__':
