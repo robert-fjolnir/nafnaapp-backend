7fd2a67f3ad76d5fc2d17eaf91aece290a6c0004
Róbert Fjölnir Birkisson <rfb97@hotmail.com>
2025-02-13 15:06:27 +0000

    Populated baby names

--- app/main.py
+++ app/main.py
@@ -33,3 +33,51 @@ def create_user(email: str, password: str, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+@app.get("/names")
+def read_names():
+    db = SessionLocal()
+    names = db.query(models.Name).all()
+    return names
+
+@app.post("/populate_names")
+def populate_names():
+    db = SessionLocal()
+
+    print("Starting to populate names")
+    
+    with open('names/kk.txt', 'r', encoding='utf-8') as file:
+        male_names = file.read().splitlines()
+        for name in male_names:
+            db_name = models.Name(name=name, category=models.CategoryEnum.KARLKYNS)
+            db.add(db_name)
+        db.commit()
+        print("Finished with male names")
+        
+    
+    with open('names/kvk.txt', 'r', encoding='utf-8') as file:
+        female_names = file.read().splitlines()
+        for name in female_names:
+            db_name = models.Name(name=name, category=models.CategoryEnum.KVENKYNS)
+            db.add(db_name)
+        db.commit()
+        print("Finished with female names")
+
+    with open('names/hvk.txt', 'r', encoding='utf-8') as file:
+        neutral_names = file.read().splitlines()
+        for name in neutral_names:
+            db_name = models.Name(name=name, category=models.CategoryEnum.KYNHLUTLAUS)
+            db.add(db_name)
+        db.commit()
+        print("Finished with neutral names")
+    
+    with open('names/milli.txt', 'r', encoding='utf-8') as file:
+        middle_names = file.read().splitlines()
+        for name in middle_names:
+            db_name = models.Name(name=name, category=models.CategoryEnum.MILLINOFN)
+            db.add(db_name)
+        db.commit()
+        print("Finished with middle names")
+    
+    return {"message": "Names populated"}
--- app/models.py
+++ app/models.py
@@ -1,9 +1,55 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from sqlalchemy.types import Enum
+from app.database import Base
+import enum
 
+# TYPES
+class CategoryEnum(enum.Enum):
+    KARLKYNS = "karlkyns"
+    KVENKYNS = "kvenkyns"
+    KYNHLUTLAUS = "kynhlutlaust"
+    MILLINOFN = "milli"
+
+
+# DATABASE MODELS
 class User(Base):
     __tablename__ = "users"
-
+    
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
+    # Relationship with partner (another user)
+    partner_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    partner = relationship("User", remote_side=[id])
+    
+    # Relationships
+    swipes = relationship("Swipe", back_populates="user")
+
+class Name(Base):
+    __tablename__ = "names"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    category = Column(Enum(CategoryEnum), nullable=False)
+    # origin = Column(String, nullable=True)
+    # meaning = Column(String, nullable=True)
+    
+    # Relationships
+    swipes = relationship("Swipe", back_populates="name")
+
+class Swipe(Base):
+    __tablename__ = "swipes"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    name_id = Column(Integer, ForeignKey("names.id"))
+    liked = Column(Boolean)
+    swiped_at = Column(DateTime(timezone=True), server_default=func.now())
+    
+    # Relationships
+    user = relationship("User", back_populates="swipes")
+    name = relationship("Name", back_populates="swipes")
